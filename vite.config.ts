a7ff9c8484426cb06233e01d8724cb0a2be13a4e
hossein <h@h.com>
2025-04-18 18:29:55 +0330

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
   plugins: [react()],
+  base: 'index.html',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },

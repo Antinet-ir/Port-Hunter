4baad9a3548ac99c5656b553aebab98c44e18c22
Hossein Masihi <118922701+hosseinMsh@users.noreply.github.com>
2025-04-19 08:20:05 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     rollupOptions: {
-      external: ['framer-motion'], // If you want to exclude framer-motion from the bundle
+      external: ['framer-motion'],
     },
   },
 });
